1ba55652ac3c04cac59e81f819b2163162c66ca2
diegodev9 <diegodev9@gmail.com>
2023-11-21 07:25:42 -0300

    discussion show with inline editing

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,6 +1,6 @@
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_discussion, only: %i[edit update destroy]
+  before_action :set_discussion, only: %i[show edit update destroy]
 
   def index
     @discussions = Discussion.all
@@ -10,12 +10,16 @@ class DiscussionsController < ApplicationController
     @discussion = Discussion.new
   end
 
+  def show
+
+  end
+
   def create
     @discussion = Discussion.new(discussion_params)
 
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to discussions_path, notice: 'Discussion successfully created' }
+        format.html { redirect_to @discussion, notice: 'Discussion successfully created' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -28,7 +32,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        format.html { redirect_to discussions_path, notice: 'Discussion successfully updated' }
+        format.html { redirect_to @discussion, notice: 'Discussion successfully updated' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
