5ab2b69ce7d6a31749801b725eea1ec8ff0bc7e3
diegodev9 <diegodev9@gmail.com>
2023-11-23 09:11:42 -0300

    add new post on discussion show view

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root 'main#index'
-  resources :discussions
+  resources :discussions do
+    resources :posts, only: :create, module: :discussions
+  end
 end
