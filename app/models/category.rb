0b82d5e7db7b866ca27576a83f43fd70bfbe3dff
diegodev9 <diegodev9@gmail.com>
2023-11-28 09:06:50 -0300

    adding categories to discussion index

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ApplicationRecord
   has_many :discussions, dependent: :nullify
 
+  after_create_commit -> { broadcast_prepend_to 'categories' }
+  after_update_commit -> { broadcast_replace_to 'categories' }
+  after_destroy_commit -> { broadcast_remove_to 'categories' }
+
   scope :sorted, -> { order(name: :asc) }
 end
